9083ac36da782b4a9bf2a072870ef6dc8907e123
Kis <aranka.kis@stud.ubbcluj.ro>
2023-04-01 15:39:53 +0300

    lab2_2

--- jatek.js
+++ jatek.js
@@ -1,10 +1,19 @@
-let felhasznalo = {
-  nev: "",
-  penz: 0,
-  tipp: "",
-};
+//lementem a form-ba beirt jatekos nevet es penzet, majd atvaltok a masik html oldalra
+function masikOldal() {
+  const becenev = document.getElementById("becenev").value;
+  const penz = document.getElementById("penz").value;
+  localStorage.setItem("becenev", becenev);
+  localStorage.setItem("penz", penz);
+  
+  window.location.href = "./game.html";
+}
 
-let gep = {
-  penz: Math.floor(Math.random() * 1000) + 100,
-  tipp: "",
-};
+//megnezem, hogy lettek-e beirva adatok a formba, ha igen, megjelenitem oket a masik html oldalon
+function betolt() {
+  if (localStorage.becenev && localStorage.penz) {
+    const becenev = localStorage.getItem("becenev");
+    const penz = localStorage.getItem("penz");
+    document.getElementById("becenevjs").textContent = `Játékos neve: ${becenev}`;
+    document.getElementById("penzjs").textContent = `Játékos pénze: ${penz}`;
+  }
+}
