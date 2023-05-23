4837e09986486623bdf3341c21db4b8b484161e7
Kis <aranka.kis@stud.ubbcluj.ro>
2023-05-23 13:25:39 +0300

    lab5_2

--- public/main.js
+++ public/main.js
@@ -1,18 +1,26 @@
 async function showDetails(kod) {
-  const result = await fetch(`./fooldal/${kod}`, { method: 'GET' });
-  const bodyJson = await result.json();
-  const details = JSON.stringify(bodyJson[0]);
-  const mainPage = document.getElementById(`details-${kod}`);
-  mainPage.innerHTML = details;
-}
-
-async function deleteByName(nev) {
-  const result = await fetch(`./allomany/${nev}`, { method: 'DELETE' });
-  if (result.status === 200) {
-    document.getElementById(`allomany-${nev}`).remove();
-    alert('Az allomany sikeresen torolve lett');
+  try {
+    const result = await fetch(`./fooldal/${kod}`, { method: 'GET' });
+    const bodyJson = await result.json();
+    const details = JSON.stringify(bodyJson[0]);
+    const mainPage = document.getElementById(`details-${kod}`);
+    mainPage.innerHTML = details;
+  } catch (error) {
+    console.error('Hiba tortent a reszletek lekerese kozben:', error);
   }
 }
 
-showDetails();
-deleteByName();
+async function deleteByName(nev) {
+  try {
+    const result = await fetch(`./allomany/${nev}`, { method: 'DELETE' });
+    if (result.status === 200) {
+      document.getElementById(`allomany-${nev}`).remove();
+      alert('Az allomany sikeresen torolve lett');
+    }
+  } catch (error) {
+    console.error('Hiba tortent az allomany torlesekor:', error);
+  }
+}
+
+showDetails('kod');
+deleteByName('nev');
