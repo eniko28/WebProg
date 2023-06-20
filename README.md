667dc14f26c906c3596e256e0f4db8975f547a94
Kis <aranka.kis@stud.ubbcluj.ro>
2023-06-20 19:57:18 +0300

    projekt20:)

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Webprogramozás laborfeladatok
 
-Mivel nincs regisztrációra lehetőség, az admint a tábla létrehozáskor beszúrtam a táblába.
-Felhasználóneve: admin01
-Jelszó: alma
+# Mivel nincs regisztrációra lehetőség, az admint a tábla létrehozáskor beszúrtam a táblába.
+
+# Felhasználónév: admin01
+
+# Jelszó: alma
--- routes/adatok.js
+++ routes/adatok.js
@@ -22,14 +22,19 @@ router.post('/adatok', async (req, res) => {
   try {
     const ujNev = req.fields.nev;
     const regiNev = req.fields.nevRegi;
+    const bevezetettNevek = await dbfelhasznalo.selectUsers();
     // az uj felhasznalonevnek kulonboznie kell az aktualistol
-    if (ujNev !== regiNev) {
-      await dbfelhasznalo.updateFelhasznalo(ujNev, regiNev);
-      res.redirect(`/adatok?felhasznalo=${ujNev}&action=Vendég`);
+    if (ujNev === regiNev) {
+      res.status(500).render('error', { message: 'Az új felhasználónévnek különböznie kell a regitől!' });
       return;
     }
-    res.status(500).render('error', { message: 'Az új felhasználónévnek különböznie kell a regitől!' });
-    return;
+    const ok = bevezetettNevek.some((obj) => obj.nev.includes(ujNev));
+    if (ok) {
+      res.status(400).render('error', { message: 'A megadott névvel már van felhasználó regisztrálva!' });
+      return;
+    }
+    await dbfelhasznalo.updateFelhasznalo(ujNev, regiNev);
+    res.redirect(`/adatok?felhasznalo=${ujNev}&action=Vendég`);
   } catch (err) {
     res.status(500).render('error', { message: `Hiba:  ${err.message}` });
   }
